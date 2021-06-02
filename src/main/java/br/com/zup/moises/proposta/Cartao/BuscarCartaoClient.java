ee178baab0c17260d3af5cf9020dbdbeb84bb260
MoisesWesley <moises.wesley.s@gmail.com>
2021-06-02 11:17:31 -0300

    040.rodar_nossa_aplicacao com erro
    Dockerizando aplicação, porem com erro haha
    testar....

--- src/main/java/br/com/zup/moises/proposta/Cartao/BuscarCartaoClient.java
+++ src/main/java/br/com/zup/moises/proposta/Cartao/BuscarCartaoClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "vincular-Cartao", url = "http://localhost:8888")
+@FeignClient(name = "vincular-Cartao", url = "http://contas:8888")
 public interface BuscarCartaoClient {
 	@GetMapping("/api/cartoes")
 	SolicitaCartaoResponse dadosCartao(@RequestParam(name="idProposta") String idProposta);
